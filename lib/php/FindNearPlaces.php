6b628a93725f1c0f5272d232c4c1382b3aa495db
unknown <satyasri.chintapalli@gmail.com>
2022-11-11 15:07:52 +0000

    Final Push

--- lib/php/FindNearPlaces.php
+++ lib/php/FindNearPlaces.php
@@ -2,26 +2,55 @@
   ini_set('display_errors' , 'On');
   error_reporting(E_ALL);
 
-  $lat = $_REQUEST['lat'];
-  $lng = $_REQUEST['lng'];
-  $url = 'http://api.geonames.org/findNearbyPlaceNameJSON?formatted=true&lat='.$lat.'&lng='. $lng.'&username=venkysatya&style=full';
+ 
+
+  $executionStartTime = microtime(true);
+  
+  $url = 'http://api.geonames.org/findNearbyPlaceNameJSON?formatted=true&lat='.$_REQUEST['lat'].'&lng='. $_REQUEST['lng'].'&username=venkysatya&style=full';
   
   
   $ch = curl_init();
 
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false );
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-  
-  $result = curl_exec($ch);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-  curl_close($ch);
-  
-  $decode = json_decode($result,true);
+    curl_setopt($ch, CURLOPT_URL,$url);
 
-  $output['data'] = $decode['geonames'];
-  
-  header('Content-Type: application/json; charset=UTF-8');
-  echo json_encode ($output);
+   
+
+ 
+
+    $result=curl_exec($ch);
+
+ 
+
+    curl_close($ch);
+
+ 
+
+    $decode = json_decode($result,true);    
+
+ 
+
+    $output['status']['code'] = "200";
+
+    $output['status']['name'] = "ok";
+
+    $output['status']['description'] = "success";
+
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+
+    $output['data'] = $decode['geonames'];
+
+   
+
+    header('Content-Type: application/json; charset=UTF-8');
+
+ 
+
+    echo json_encode($output);
+
+ 
+
 ?>
